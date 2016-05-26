0e02b4717a9cabea76813b6eb7f4d1a346acf589
Brian Doane <brianmdoane@gmail.com>
2016-05-26 10:32:39 -0500

    added nav, footer has issues

--- js/employees.js
+++ js/employees.js
@@ -45,15 +45,23 @@ for (var i = 0; i < sunEmployees.length; i++) {
 	var selectedEmployee = sunEmployees[i];
 
 	employeeBio.innerHTML += 
-		"<div class='employee'>" +
-			"<div class='eName'>" + selectedEmployee.name + "</div>" +
-			"<div class='eTitle'>" + selectedEmployee.title + "</div>" +
-			"<img class='eImage' src='" + selectedEmployee.image + "'>" + 
-			"<div class='eDescription'>" + selectedEmployee.description + 
-			"</div>" +
-		"</div>";
+		`<div class='employee'>
+			<div class='eName'>${selectedEmployee.name}</div>
+			<div class='eTitle'>${selectedEmployee.title}</div>
+			<img class='eImage' src='${selectedEmployee.image}'>
+			<div class='eDescription'>${selectedEmployee.description}</div>
+		</div>`;
 };
 
+// ES6 vs "Traditional"
+// employeeBio.innerHTML += 
+// 	"<div class='employee'>" +
+// 		"<div class='eName'>" + selectedEmployee.name + "</div>" +
+// 		"<div class='eTitle'>" + selectedEmployee.title + "</div>" +
+// 		"<img class='eImage' src='" + selectedEmployee.image + "'>" + 
+// 		"<div class='eDescription'>" + selectedEmployee.description + 
+// 		"</div>" +
+// 	"</div>";
 
 
 
